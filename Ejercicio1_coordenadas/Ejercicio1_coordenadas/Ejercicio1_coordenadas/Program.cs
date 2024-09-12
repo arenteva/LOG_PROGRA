d0313bb1f0b922007115b9108da9873f3d8596a2
arenteva <retehalo2@hotmail.com>
2024-09-12 04:52:31 -0500

    Se calcularon las variables

--- Ejercicio1_coordenadas/Ejercicio1_coordenadas/Ejercicio1_coordenadas/Program.cs
+++ Ejercicio1_coordenadas/Ejercicio1_coordenadas/Ejercicio1_coordenadas/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿// Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos
+
+using System.Numerics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Datos de Entrada
+        //Declarar las variables en este caso son 4:
+        double x1, y1, x2, y2, d;
+
+        //Vamos a inicializar las variables pidiendoles sus valores por pantalla:
+        Console.Write("Por favor ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Por favor ingrese el valor de y1:  ");
+        y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Por favor ingrese el valor de x2:  ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Por favor ingrese el valor de y2:  ");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        //Proceso - calcular la variable d
+        double x = (x2 - x1) * (x2 - x1);
+        double y = (y2 - y1) * (y2 - y1);
+
+
     }
+
+  
 }
