e1ae4c17dceb3cea63add3d7fafd5f7c0dc34b1d
arenteva <retehalo2@hotmail.com>
2024-10-07 06:07:06 -0500

    Codigo terminado y funcioando

--- Ejercicio22_IMC_For/Ejercicio22_IMC_For/Ejercicio22_IMC_For/Program.cs
+++ Ejercicio22_IMC_For/Ejercicio22_IMC_For/Ejercicio22_IMC_For/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -6,6 +8,35 @@
 
         //Datos de entrada
         int num1 , num2 ;
+        int menor, mayor;
+
+        Console.Write("Ingrese el primer numero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("\nIngrese el segundo numero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 < num2)
+        {
+            menor = num1;
+            mayor = num2;
+           
+        }
+        else
+        {
+            mayor = num1;
+            menor = num2;
+        }
+        //Vamos a contruir el for
+
+        Console.WriteLine($"\nLos numeros comprendidos entre {menor} y {mayor} son:");
+
+        for (int i = menor; i <= mayor; i++)
+        {    
+            Console.Write($"{i}  "); 
+        }
+        
 
 
     }
