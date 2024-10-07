09fb2cc76bb33731f1138de5bea9f9bde87692f1
arenteva <retehalo2@hotmail.com>
2024-10-07 05:08:05 -0500

    ejercicio numero 15

---
+++ Ejercicio22_IMC_For/Ejercicio22_IMC_For/Ejercicio22_IMC_For/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //15.Programe un algoritmo que pida a usuario 2 números por pantalla y que muestre todos los números comprendidos entre ambos números incluyéndolos.//
+
+        //Datos de entrada
+        int num1 , num2 ;
+
+
+    }
+}
